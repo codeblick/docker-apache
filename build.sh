07821f032564f6ab5c0144d8edd2a6bfb5821595
Johannes Berthel <j.berthel@codeblick.de>
2018-11-30 14:56:55 +0100

    remove le support

--- build.sh
+++ build.sh
@@ -5,8 +5,3 @@ docker build apache \
     -t codeblick/apache:2.4 \
     --build-arg COB_HTTPD_VERSION=2.4 \
     -q
-
-docker build apache-le \
-    -t codeblick/apache:2.4-le \
-    --build-arg COB_HTTPD_VERSION=2.4 \
-    -q
--- push.sh
+++ push.sh
@@ -4,4 +4,3 @@ set -e
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
 docker push codeblick/apache:2.4
-docker push codeblick/apache:2.4-le
